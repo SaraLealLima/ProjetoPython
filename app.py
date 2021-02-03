82a2366252813668aaf81b088fb21f37d8cb1244
SaraLealLima <saral.limasm@gmail.com>
2021-02-02 21:32:37 -0300

    validação de dados

--- app.py
+++ app.py
@@ -1,59 +1,72 @@
 from flask import Flask, render_template, request, send_from_directory
 
+
 app = Flask(__name__, template_folder="")
+
 
 @app.route("/", methods = ["GET", "POST"])
 def home():
     if (request.method == "GET"):
         return render_template("index.html")
     else:
-        if (request.form["num1"] != "" and request.form["num2"] != ""):
-            num1 = request.form["num1"]
-            num2 = request.form["num2"]
+        if (request.form["num1"] == ""):
+            return "Preencha o primeiro campo"
 
-            if (request.form["calculo"] == "soma"):
-                soma = int(num1) + int(num2)
-                return {
-                    "Resultado": str(soma) 
-                }
-                
-            elif (request.form["calculo"] == "subtracao"):
-                subtracao = int(num1) - int(num2)
-                return {
-                    "Resultado": str(subtracao) 
-                }
-            elif (request.form["calculo"] == "multiplicacao"):
-                multiplicacao = int(num1) * int(num2)
-                return {
-                    "Resultado": str(multiplicacao) 
-                }
-            elif (request.form["calculo"] == "juros"):
-                juros1 = int(num2) / 100
-                juros2 = int(num1) * juros1
-                juros3 = int(num1) + juros2
-                return {
-                    "Resultado": str(juros3) 
-                }   
-            elif (request.form["calculo"] == "divisao"):
-                divisao = int(num1) // int(num2)
-                return {
-                    "Resultado": str(divisao) 
-                }
-            else:
-                resto = int(num1) % int(num2)
-                return {
-                    "Resultado": str(resto) 
-                }
+        if (request.form["num2"] == ""):
+            return "Preencha o segundo campo"
+            
+        if (request.form["calculo"] == ""):
+            return "Escolha uma operação"
+
+             
+        num1 = request.form["num1"]
+        num2 = request.form["num2"]
+
+        if (request.form["calculo"] == "soma"):
+            soma = int(num1) + int(num2)
+            return {
+                "Resultado": str(soma) 
+            }
+            
+        elif (request.form["calculo"] == "subtracao"):
+            subtracao = int(num1) - int(num2)
+            return {
+                "Resultado": str(subtracao) 
+            }
+        elif (request.form["calculo"] == "multiplicacao"):
+            multiplicacao = int(num1) * int(num2)
+            return {
+                "Resultado": str(multiplicacao) 
+            }
+        elif (request.form["calculo"] == "juros"):
+            juros1 = int(num2) / 100
+            juros2 = int(num1) * juros1
+            juros3 = int(num1) + juros2
+            return {
+                "Resultado": str(juros3) 
+            }   
+        elif (request.form["calculo"] == "divisao"):
+            divisao = int(num1) // int(num2)
+            return {
+                "Resultado": str(divisao) 
+            }
         else:
-            return "Informe valores válidos"
+            resto = int(num1) % int(num2)
+            return {
+                "Resultado": str(resto) 
+            }
+
+            
 
 @app.route('/js/<path:path>')
 def send_js(path):
     return send_from_directory('js', path)
 
+
 @app.route('/css/<path:path>')
 def send_css(path):
     return send_from_directory('css', path)
+
 
 @app.errorhandler(404)
 def not_found(erro):
@@ -62,4 +75,5 @@ def not_found(erro):
 
 
 app.run(debug=True) 
+
 # mudar a porta: passar o parametro port=0000
